7b88e6f8d30aa4f29388ef9735ed44daae572f89
HSinhori <sinhoripedro@hotmail.com>
2022-06-24 15:08:29 -0300

    Pessoa Class Creation

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,4 +1,15 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
+    var nome: String = "Henrique"
+    var cpf: String = "123.123.123-12"
+}
+
+fun main(){
+
+    val henrique = Pessoa()
+
+    println(henrique.nome)
+    println(henrique.cpf)
+
 }
